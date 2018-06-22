0825c1c4a5031afa3960b972cdb7a2360383e02f
probuse <nprobuse@gmail.com>
2018-06-22 20:39:10 +0300

    added database configuration

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
+mongoose.Promise = global.Promise;
+
+// connect to database
+mongoose.connect(dbConfig.url).then(() => {
+    console.log("Successfully connected to database");
+}).catch(() => {
+    console.log('Could not connect to the database, Exiting now...')
+});
 
 // parse requests of content-type -application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
